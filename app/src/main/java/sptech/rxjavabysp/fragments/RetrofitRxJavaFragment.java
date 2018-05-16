dc4338bec16e53353af3f3aa8b43ae6b5484feec
SIBAPRASAD <neosoft.sibaprasad@angelbroking.com>
2018-05-16 13:42:50 +0530

    Added Network Call

--- app/src/main/java/sptech/rxjavabysp/fragments/RetrofitRxJavaFragment.java
+++ app/src/main/java/sptech/rxjavabysp/fragments/RetrofitRxJavaFragment.java
@@ -7,16 +7,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 import sptech.rxjavabysp.R;
 import sptech.rxjavabysp.network.RetrofitApiService;
 import sptech.rxjavabysp.network.RetrofitHelper;
+import sptech.rxjavabysp.network.dto.CityResponse;
+import sptech.rxjavabysp.network.dto.Geoname;
 
 public class RetrofitRxJavaFragment extends Fragment {
 
     private static final String TAG = "RetrofitRxJavaFragment";
 
     private RetrofitApiService retrofitApiService;
+    @NonNull
+    private CompositeDisposable mCompositeDisposable = new CompositeDisposable();
+
+    private TextView tvResponse;
 
     public RetrofitRxJavaFragment() {
         // Required empty public constructor
@@ -36,8 +51,7 @@ public class RetrofitRxJavaFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        retrofitApiService  = new RetrofitHelper().getCityService();
-
+        retrofitApiService = new RetrofitHelper().getCityService();
 
 
     }
@@ -49,6 +63,8 @@ public class RetrofitRxJavaFragment extends Fragment {
 // https://github.com/matthiasbruns/rxandroid2-retrofit2
         View rootView = inflater.inflate(R.layout.fragment_retrofit_rx_java, container, false);
 
+        tvResponse = rootView.findViewById(R.id.tvResponse);
+        requestGeonames();
         return rootView;
     }
 
@@ -58,4 +74,37 @@ public class RetrofitRxJavaFragment extends Fragment {
 
     }
 
+    private void displayGeonames(@NonNull final List<Geoname> geonames) {
+        // Cheap way to display a list of Strings - I was too lazy to implement a RecyclerView
+        final StringBuilder output = new StringBuilder();
+        for (final Geoname geoname : geonames) {
+            output.append(geoname.name).append("\n");
+        }
+
+        tvResponse.setText(output.toString());
+    }
+
+    private void requestGeonames() {
+        mCompositeDisposable.add(retrofitApiService.queryGeonames(44.1, -9.9, -22.4, 55.2, "de")
+                .subscribeOn(Schedulers.io()) // "work" on io thread
+                .observeOn(AndroidSchedulers.mainThread()) // "listen" on UIThread
+                .map(new Function<CityResponse, List<Geoname>>() {
+                    @Override
+                    public List<Geoname> apply(
+                            @io.reactivex.annotations.NonNull final CityResponse cityResponse)
+                            throws Exception {
+                        // we want to have the geonames and not the wrapper object
+                        return cityResponse.geonames;
+                    }
+                })
+                .subscribe(new Consumer<List<Geoname>>() {
+                    @Override
+                    public void accept(
+                            @io.reactivex.annotations.NonNull final List<Geoname> geonames)
+                            throws Exception {
+                        displayGeonames(geonames);
+                    }
+                })
+        );
+    }
 }
